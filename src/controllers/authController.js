beb6a0865309969fa0382441088462347d5533d0
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:53:53 +0300

    Add src/controllers/authController.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,27 @@
+﻿const AuthService = require('../services/authService');
+const { formatResponse } = require('../utils/helpers');
+
+class AuthController {
+  static async register(req, res, next) {
+    try {
+      const result = await AuthService.register(req.validatedBody || req.body);
+      res.status(201).json(formatResponse(result));
+    } catch (error) { next(error); }
+  }
+
+  static async login(req, res, next) {
+    try {
+      const result = await AuthService.login(req.validatedBody || req.body);
+      res.json(formatResponse(result));
+    } catch (error) { next(error); }
+  }
+
+  static async getProfile(req, res, next) {
+    try {
+      const user = await AuthService.getProfile(req.user.id);
+      res.json(formatResponse(user));
+    } catch (error) { next(error); }
+  }
+}
+
+module.exports = AuthController;
