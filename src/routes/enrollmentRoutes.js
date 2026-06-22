9eefa49a70cd40288a348d692f1d585002fc5a0a
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:54:08 +0300

    Add src/routes/enrollmentRoutes.js

---
+++ src/routes/enrollmentRoutes.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const router = express.Router();
+const EnrollmentController = require('../controllers/enrollmentController');
+const authenticate = require('../middleware/auth');
+
+router.post('/', authenticate, EnrollmentController.enroll);
+router.get('/my', authenticate, EnrollmentController.getMyEnrollments);
+router.delete('/:id', authenticate, EnrollmentController.unenroll);
+
+module.exports = router;
