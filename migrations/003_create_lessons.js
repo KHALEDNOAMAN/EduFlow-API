432972ea47b2a45fd1f931802983052af71b9a33
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:53:43 +0300

    Add migrations/003_create_lessons.js

---
+++ migrations/003_create_lessons.js
@@ -0,0 +1,16 @@
+﻿exports.up = function(knex) {
+  return knex.schema.createTable('lessons', (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.uuid('course_id').notNullable().references('id').inTable('courses').onDelete('CASCADE');
+    table.string('title', 200).notNullable();
+    table.text('content');
+    table.string('video_url', 500);
+    table.integer('order_index').notNullable().defaultTo(0);
+    table.integer('duration_minutes').notNullable().defaultTo(30);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('lessons');
+};
