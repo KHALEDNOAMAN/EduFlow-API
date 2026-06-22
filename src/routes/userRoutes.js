050830693c995b0acc254dd4fd588b271fc0d896
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:54:11 +0300

    Add src/routes/userRoutes.js

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/userController');
+const authenticate = require('../middleware/auth');
+const authorize = require('../middleware/roleMiddleware');
+
+router.get('/', authenticate, authorize('admin'), UserController.getAll);
+router.get('/:id', authenticate, authorize('admin'), UserController.getById);
+router.put('/:id', authenticate, authorize('admin'), UserController.update);
+router.delete('/:id', authenticate, authorize('admin'), UserController.delete);
+
+module.exports = router;
