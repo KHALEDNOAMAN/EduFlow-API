da30e3e5782617381cff64f737d1fe859026edea
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:54:02 +0300

    Add src/middleware/roleMiddleware.js

---
+++ src/middleware/roleMiddleware.js
@@ -0,0 +1,15 @@
+﻿const AppError = require('../utils/AppError');
+
+const authorize = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return next(new AppError('Authentication required', 401, 'AUTH_REQUIRED'));
+    }
+    if (!allowedRoles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403, 'FORBIDDEN'));
+    }
+    next();
+  };
+};
+
+module.exports = authorize;
