10f0dec37d4e6d58e53733e3c0b1bfd976b747a1
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:53:46 +0300

    Add migrations/005_create_configurations.js

---
+++ migrations/005_create_configurations.js
@@ -0,0 +1,13 @@
+﻿exports.up = function(knex) {
+  return knex.schema.createTable('configurations', (table) => {
+    table.increments('id').primary();
+    table.string('key', 100).notNullable().unique();
+    table.text('value');
+    table.text('description');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('configurations');
+};
