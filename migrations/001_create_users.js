67b9e7f51983dd8e78cbee23dc2f78b2d34d83d8
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 01:53:41 +0300

    Add migrations/001_create_users.js

---
+++ migrations/001_create_users.js
@@ -0,0 +1,16 @@
+﻿exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid());
+    table.string('email', 255).notNullable().unique();
+    table.string('password_hash', 255).notNullable();
+    table.string('first_name', 100).notNullable();
+    table.string('last_name', 100).notNullable();
+    table.enu('role', ['admin', 'instructor', 'student']).defaultTo('student');
+    table.boolean('is_active').defaultTo(true);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
